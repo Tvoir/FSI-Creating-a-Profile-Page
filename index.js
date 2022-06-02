36328de9db149eaab0f4a58f857f38cec39c68d4
Tvoir <TommyVoir@yahoo.com>
2022-06-01 21:32:50 -0700

    finished to the end.

--- index.js
+++ index.js
@@ -13,10 +13,42 @@ content.append(dogContent)
 let dogImage = document.createElement('img')
 dogImage.setAttribute('class', 'dog-image')
 dogImage.setAttribute('src', './assets/rizzo.jpg')
-content.append(dogImage)
+dogContent.append(dogImage)
 
 let dogDetails = document.createElement('div')
 dogDetails.setAttribute('class', 'dog-details')
-content.append(dogDetails)
+dogContent.append(dogDetails)
+
+let headerT = document.createElement('h3')
+headerT.textContent ='Description:'
+dogDetails.append(headerT)
+
+let bodyP = document.createElement('p')
+bodyP.textContent ='This gentle dog is aloof toward her owner, and never comes when called. She always acts as though any stranger she meets will harm her, and dislikes other animals.'
+dogDetails.append(bodyP)
+
+let HeaderTimes = document.createElement('h3')
+HeaderTimes.textContent ='Feeding Times:'
+dogDetails.append(HeaderTimes)
 
 
+
+
+let list = document.createElement('ul')
+
+let timeA = document.createElement('li')
+timeA.textContent ='9:00 am'
+list.append(timeA)
+
+let timeB = document.createElement('li')
+timeB.textContent ='12:00 pm'
+list.append(timeB)
+
+let timeC = document.createElement('li')
+timeC.textContent ='5:00 pm'
+list.append(timeC)
+dogDetails.append(list)
+
+let body2 = document.createElement('p')
+body2.textContent ='hello'
+dogDetails.append(body2)
